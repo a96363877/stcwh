6662e4aaf571f8f4a84b97c349ccc93c2d29d92c
a96363877 <a96363877@gmail.com>
2025-05-18 03:54:47 +0300

    asd

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -11,13 +11,13 @@ import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
   // Your Firebase configuration will be injected here
-  apiKey: 'AIzaSyAD3iyMWhdzQ4VIXZcwCpUJTnqFTe5jt7U',
-  authDomain: 'wedsdasd.firebaseapp.com',
-  projectId: 'wedsdasd',
-  storageBucket: 'wedsdasd.firebasestorage.app',
-  messagingSenderId: '299161995646',
-  appId: '1:299161995646:web:45b8e58faa99d3e75ccb2f',
-  measurementId: 'G-614JDKQGMC',
+  apiKey: "AIzaSyBalb45hYmw3rGK3kn5Skp2Wb4Ci3yeKHc",
+  authDomain: "adsapp-for.firebaseapp.com",
+  projectId: "adsapp-for",
+  storageBucket: "adsapp-for.firebasestorage.app",
+  messagingSenderId: "282166621415",
+  appId: "1:282166621415:web:2be67338bc64233153de42",
+  measurementId: "G-PG5NPD4FES"
 };
 
 const app = initializeApp(firebaseConfig);
